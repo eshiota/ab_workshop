602a21195ed4c8a6564c6e6231a8d5315203be6d
Eduardo Shiota Yasuda <yl.edu.sy@gmail.com>
2015-10-14 10:01:40 -0500

    Moving experiment loading logic to Split

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 
 gem 'jquery-rails'
-gem 'split', require: 'split/dashboard'
+gem 'split', require: 'split/dashboard', path: '/Users/eshiota/Sites/oss/split'
 gem 'sass-globbing'
 
 group :development do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,24 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :fetch_experiments_from_file, :assign_variants_on_all_experiments
-
-  def fetch_experiments_from_file
-    @experiments_list ||= parse_experiments_file
-    @experiments = @experiments_list.map { |name| Split::ExperimentCatalog::find_or_create name }
-  end
-
-  def assign_variants_on_all_experiments
-    @experiments_variants = @experiments.map do |experiment|
-        trial = Split::Trial.new(user: ab_user, experiment: experiment, override: override_alternative(experiment.name), exclude: exclude_visitor?, disabled: split_generically_disabled?)
-        alt = trial.choose!
-        alt ? alt.name : nil
-    end
-  end
-
-  private
-
-  def parse_experiments_file
-    YAML.load_file("config/experiments.yml").keys
-  end
+  before_action :assign_variants_to_user
 end
