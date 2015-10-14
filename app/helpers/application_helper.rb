4f315f83787c7ddbae60a147c8d46f98d4b1d665
Eduardo Shiota Yasuda <yl.edu.sy@gmail.com>
2015-10-14 10:36:15 -0500

    Add JS experiment data to view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def js_user_experiments_data
+    output = ["<script>window.B = window.B || {}; B.jsExpData = {};"]
+    @user_experiments.each do |key, value|
+      output.push "B.jsExpData['#{key}'] = '#{escape_javascript value}';"
+    end
+    output.push('</script>').join('').gsub(/\s/, '').html_safe
+  end
 end
